00b500df4f7a7f41a88de919d12de0c971c3c4c8
dokyanh220 <kyanhdubo2107@gmail.com>
2025-04-19 17:37:56 +0700

    Done6

--- assets/js/style.js
+++ assets/js/style.js
@@ -27,3 +27,30 @@ modal.addEventListener('click', hideshowbuytickets)
 modalcontainer.addEventListener('click', function(event) {
     event.stopPropagation(); // ngăn chặn sự kiện click từ modalContent truyền lên modal(sự kiện nổi bọt)
 })
+
+
+// sự kiện nút menu trên mobile
+var header = document.getElementById("header");
+var menu = document.getElementById("mobileMenu");
+var headerHeight = header.clientHeight; // lấy chiều cao của header
+
+menu.onclick = function() {
+    var isClosed = header.clientHeight === headerHeight; 
+    // kiểm tra chiều cao của header có bằng chiều cao ban đầu không
+    // so sánh isClosed nếu bằng chiều cao của header thì mở menu, ngược lại thì đóng menu
+    if (isClosed) {
+        header.style.height = "auto";
+    } 
+    else {
+        header.style.height = null;
+    }
+}
+
+// khi click vào menu thì sẽ đóng menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');{
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].onclick = function() {
+            header.style.height = null; // đóng menu khi click vào menu
+        }
+    }
+}
